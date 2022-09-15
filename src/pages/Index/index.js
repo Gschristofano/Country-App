ca8d3a9490cd5bfb69ccebe1447faa27f0438942
Gschristofano <gschristofano@gmail.com>
2022-09-15 12:31:15 -0300

    initial commit

---
+++ src/pages/Index/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { H1 } from "./styles";
+
+
+export default function Index(){
+
+
+    return (
+        <H1>Hello Word</H1>
+
+
+    );
+}
---
+++ src/pages/Index/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+
+
+
+export const H1 = styled.h1`
+    font-size: ${(props) => props.font ? props.font : 30}px;
+   
+`
